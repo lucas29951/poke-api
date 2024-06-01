fc22bc3fa101f6b0228b535fd9e3841b1dd94a95
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-06-01 20:25:36 -0300

    Funcion agregada para consumir la API

--- assets/scripts/pokemon.js
+++ assets/scripts/pokemon.js
@@ -8,3 +8,9 @@ const notFoundMessage = document.querySelector('#not-found-message');
 
 let allPokemons = [];
 
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
+.then((response) => response.json())
+.then((data) => {
+    allPokemons = data.results;
+    console.log(data.results[0]);
+});
