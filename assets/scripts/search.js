26a6920fbe03503e1e2a45012da16db5a779ff92
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-06-02 02:29:35 -0300

    Script para el manejo del componente Search agregado

---
+++ assets/scripts/search.js
@@ -0,0 +1,18 @@
+
+const inputElement = document.querySelector('#search-input');
+const search_icon = document.querySelector('#search-close-icon');
+const sort_wrapper = document.querySelector('.sort-wrapper');
+
+inputElement.addEventListener("input", () => {
+    handleInputChange(inputElement);
+});
+
+search_icon.addEventListener("click", () => {
+    handleSearchCloseOnClick;
+});
+
+sort_wrapper.addEventListener("click", () => {
+    handleSortIconOnCLick;
+});
+
+
